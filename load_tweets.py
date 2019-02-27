2da86ca2d7c69c8863ed66517d2366acb1d03368
Richard Knudsen <richardknudsen@Richards-MacBook-Pro.local>
2019-02-27 18:40:27 +0100

    added conversion to dataframe

--- load_tweets.py
+++ load_tweets.py
@@ -15,31 +15,27 @@ def load_tweets_from_dir(dir_p, ext=None):
     return tweet_files
 
 
-#def load_tweets2df(dir_p, attrs, ext=None):
-#    """
-#    loads all files with extension ext in directory dir_p
-#    and extracts attributes into pd.Dataframe
-#
-#    :parameters:
-#    dir_p: str
-#        directory with tweets
-#    attrs: list of str
-#        attributes to be extracted from tweet object,
-#        e.g. 'id_str' or 'user.screen_name' for nested values
-#
-#    :returns:
-#    pd.Dataframe with the extracted attributes
-#    """
-#    tweets_files = load_tweets_from_dir(dir_p, ext)
-#    data = {a: [] for a in attrs}
-#    for f in tweets_files:
-#        for t in f:
-#            for a in attrs:
-#                keys = a.split('.')
-#            data[a] += [safe_get(t, *keys)]
-#    df = pd.DataFrame(data)
-#    return df
+def load_tweets2df(dir_p, attrs, ext=None):
+    """
+    loads all files with extension ext in directory dir_p
+    and extracts attributes into pd.Dataframe
 
+    :parameters:
+    dir_p: str
+        directory with tweets
+    attrs: list of str
+        attributes to be extracted from tweet object,
+        e.g. 'id_str' or 'user.screen_name' for nested values
 
-#df = pd.read_json("test_data/tweets.txt", orient = 'records', lines = True)
-#df1 = load_tweets2df("test_data", ["id_str"], ext=".txt")
+    :returns:
+    pd.Dataframe with the extracted attributes
+    """
+    tweets_files = load_tweets_from_dir(dir_p, ext)
+    data = {a: [] for a in attrs}
+    for f in tweets_files:
+        for t in f:
+            for a in attrs:
+                keys = a.split('.')
+                data[a] += [safe_get(t, *keys)]
+    df = pd.DataFrame(data)
+    return df
--- test_load_tweets.py
+++ test_load_tweets.py
@@ -1,4 +1,5 @@
 import pytest
+import pandas as pd
 from load_tweets import *
 from utils import *
 
@@ -31,3 +32,17 @@ def test_load_tweets_from_dir(test_dir_p, ext, target_fp):
     for f in tweets_files:
         for t, tar_t in zip(f, target_tweets):
             assert t == t
+
+
+@pytest.mark.parametrize(
+    'attrs,tar_df_p', [
+        [
+            ('id_str', 'user.screen_name', 'retweeted', 'bla'),
+            'test_data/target_tweets_df.pickle'
+        ]
+    ]
+)
+def test_load_tweets2df(test_dir_p, attrs, tar_df_p, ext):
+    df = load_tweets2df(test_dir_p, attrs, ext)
+    df_tar = pd.read_pickle(tar_df_p)
+    assert df.equals(df_tar)
