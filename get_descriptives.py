94364c96ce31499cf94f6b2c89b4b32ed05c649a
Nandan <nandanmarkrao@gmail.com>
2019-05-06 23:04:22 +0200

    progress on reductions

--- get_descriptives.py
+++ get_descriptives.py
@@ -4,27 +4,41 @@ from tqdm import tqdm
 from utils import *
 from functools import reduce
 from toolz import curry
+from itertools import combinations
+from copy import deepcopy
+from itertools import combinations
+
+import zlib
 
 def flatten_counts_to_df(di, l1, l2):
     return pd.DataFrame([{l1: v1, l2: v2, 'count': count}
                          for v1, values in di.items()
                          for v2, count in values.items()])
 
-def retweet_count(users, tweets):
-    picker = lambda t: t.get('retweeted_status.user.screen_name')
-    return _count_per_user(users, picker, tweets)
 
-def reply_count(users, tweets):
-    picker = lambda t: t.get('in_reply_to_screen_name')
-    return _count_per_user(users, picker, tweets)
+def all_counts(users, tweets):
+    pickers = [lambda t: t.get('user.screen_name') if _is_original(t) else None,
+               lambda t: t.get('retweeted_status.user.screen_name'),
+               lambda t: t.get('in_reply_to_screen_name'),
+               lambda t: t.get('entities.user_mentions.,screen_name')]
 
-def mention_count(users, tweets):
-    picker = lambda t: t.get('entities.user_mentions.,screen_name')
-    return _count_per_user(users, picker, tweets)
+    reducers = [_per_day(_reduce_picker(users, picker)) for picker in pickers]
+    inits = [{} for _ in pickers]
+    return multireduce(tweets, reducers, inits)
 
-def _count_per_user(users, picker, tweets):
-    reducer = _reduce_picker(users, picker)
-    return reduce(_per_day(reducer), tweets, {})
+
+def multireduce(iterable, funcs, inits):
+    def reducer(accs, t):
+        return [func(acc, t) for acc,func in zip(accs, funcs)]
+
+    return reduce(reducer, iterable, inits)
+
+def _is_original(t):
+    keys = ['in_reply_to_status_id_str',
+            'retweeted_status.id_str',
+            'quoted_status.id_str']
+
+    return not [t.get(k) for k in keys if t.get(k)]
 
 @curry
 def _reduce_picker(users, picker, acc, t):
@@ -46,26 +60,15 @@ def _update_count(users, counts, vals):
         vals = [vals]
 
     vals = [v for v in vals if v in users]
+    return reduce(_freq, vals, counts)
 
-    for val in vals:
-        try:
-            counts[val] += 1
-        except KeyError:
-            counts[val] = 1
-
+@curry
+def _freq(counts, val):
+    try:
+        counts[val] += 1
+    except KeyError:
+        counts[val] = 1
     return counts
-
-def original_count(users, tweets):
-    tweets = (t for t in tweets if t.get('user.screen_name') in users)
-    names = (t.get('user.screen_name') for t in tweets if _is_original(t))
-    return reduce(_update_count(users), names, {})
-
-def _is_original(t):
-    keys = ['in_reply_to_status_id_str',
-            'retweeted_status.id_str',
-            'quoted_status.id_str']
-
-    return not [t.get(k) for k in keys if t.get(k)]
 
 def follower_count(users, tweets):
     tweets = (t for t in tweets if t.get('user.screen_name') in users)
@@ -84,50 +87,223 @@ def _followers(keys, acc, t):
     return acc
 
 
+def make_edge_df(edges):
+    [{'users': k, 'weight': w} for k,w in edges.items()]
+    pass
 
-def get_edge_weights(tweet_df, user):
-    '''
-    for all pairwise combinations of users, this function counts how many users
-    interact with both users of the respective combination. interaction is divided into
-    reply to tweets by user, retweet tweety by user or mention user
+def get_edge_weights(users, tweets):
 
-    :param pd.DataFrame tweet_df: tweet dataframe with tweet attributes as columns
-    :param list user: target users
-    :returns pd.DataFrame res: dataframe with edge counts
-    '''
-    user_combs = pairwise_combs(user)
-    res = []
-    print('...... Getting edge weights ...')
-    for u1, u2 in tqdm(user_combs):
-        w_reply = tweet_df\
-            [tweet_df['in_reply_to_screen_name'].isin([u1, u2])]\
-            .groupby('user.screen_name')\
-            ['in_reply_to_screen_name']\
-            .nunique()\
-            .ge(2)\
-            .sum()
-        w_retweet = tweet_df\
-            [tweet_df['retweeted_status.user.screen_name'].isin([u1, u2])]\
-            .groupby('user.screen_name')\
-            ['in_reply_to_screen_name']\
-            .nunique()\
-            .ge(2)\
-            .sum()
-        # w_mention
-        idxs = outerjoin_lists(
-            get_idxs_true(isin_listofsets(u1, tweet_df['entities.user_mentions.,screen_name'])),
-            get_idxs_true(isin_listofsets(u2, tweet_df['entities.user_mentions.,screen_name'])),
-        )
-        df_fil = tweet_df.iloc[idxs]
-        df_fil = df_fil \
-            .groupby('user.screen_name') \
-            .apply(lambda x: set.union(*x['entities.user_mentions.,screen_name']))
-        w_mention = np.array(list(isin_listofsets(u1, df_fil, return_int=True))) \
-            .dot(list(isin_listofsets(u2, df_fil, return_int=True)))
-        res += [(u1, u2, w_reply, w_retweet, w_mention)]
-    res = pd.DataFrame(res)
-    res.columns = ['user1', 'user2', 'weight replies', 'weight retweets', 'weight mentions']
-    return res
+
+    pass
+
+def get_edge_weights(users, tweets):
+    edges = (_make_pairs(_get_all_names_in_tweet(users, t)) for t in tweets)
+    keys = ('{}:{}'.format(a,b) for a,b in edges)
+    return reduce(_freq, keys, {})
+
+def _make_pairs(names):
+    return combinations(sorted(names), 2)
+
+def _get_all_edges_in_tweet(users, tweet):
+
+    a = tweet.get('user.screen_name')
+    # handle missing screen_name???
+
+    # replies, retweets, mentions
+    keys = ['in_reply_to_screen_name',
+            'retweeted_status.user.screen_name',
+            'entities.user_mentions.,screen_name']
+
+    names = [tweet.get(k) for k in keys]
+    names = [n if type(n) is list else [n] for n in names if n]
+    names = [[n for n in na if n] for na in names if n in users]
+
+    # return user1, [(key, names) for key in keys]
+    return a, list(zip(keys, names))
+
+@curry
+def acc_edges(users, acc, t):
+    a, edges = _get_all_edges_in_tweet(users, t)
+    '{}:{}'
+
+# first pass through data to collect all users who mentioned at least (2) of our
+# orgs ?
+
+
+def lookup_user_ht(id_range, user):
+    return zlib.adler32(user.encode('utf8')) % id_range
+
+def lookup_user(count, lookup, user):
+    idx = lookup.get(user)
+    if idx is None:
+        lookup[user] = count
+        idx = count
+        count += 1
+    return count, lookup, idx
+
+
+def get_mentions(orgs, t):
+    keys = ['in_reply_to_screen_name',
+            'retweeted_status.user.screen_name',
+            'entities.user_mentions.,screen_name']
+
+    names = [t.get(k) for k in keys]
+    names = [n if type(n) is list else [n] for n in names]
+    names = [[n for n in k if n in orgs] for k in names]
+    mentions = list(zip([0,1,2], names))
+    return [(m,li) for m,li in mentions if li]
+
+
+@curry
+def build_array(orgs, org_lookup, acc, t):
+    user = t.get('user.screen_name')
+
+    # handle no screen_name?
+    if user is None:
+        return acc
+
+    count, lookup, arr = acc
+    count, lookup, idx = lookup_user(count, lookup, user)
+    mentions = get_mentions(orgs, t)
+
+    for m,org in mentions:
+        org_id = org_lookup[org]
+        arr[idx, m, org_id] += 1
+
+    return count, lookup, arr
+
+
+@curry
+def build_dis(orgs, org_lookup, dis, t):
+    user = t.get('user.screen_name')
+
+    # handle no screen_name?
+    if user is None:
+        return dis
+
+    mentions = get_mentions(orgs, t)
+
+    for m,orgs in mentions:
+        for org in orgs:
+            org_id = org_lookup[org]
+            try:
+                dis[m][user].add(org_id)
+            except KeyError:
+                dis[m][user] = {org_id}
+    return dis
+
+# def add_edges(edges, a):
+# for i in measures
+# for users:
+
+# user: { orgs mentioned }
+# for each user
+# for each org
+# pairs = combinations(orgs, 2)
+
+def sum_edges_di(norgs, dis):
+    # list of dictionaries, user: set of org_ids
+    edges = np.zeros((len(dis), norgs, norgs), dtype=np.int32)
+
+    for i,di in enumerate(dis):
+        for user,orgs in di.items():
+            pairs = combinations(orgs, 2)
+            for k,l in pairs:
+                edges[i, k, l] += 1
+
+    return edges
+
+def edges_to_df(orgs, keys, edges):
+
+    # make symmetric
+    edges += edges.T
+
+    x,y,z = edges.nonzero()
+
+
+    # for i in range(len(x)):
+    #     org_a, org_b = orgs[y[i]], orgs[z[i]]
+    #     key = keys[x[i]]
+    #     val = edges[x[i], y[i], z[i]]
+    #     return { key: val}
+
+
+def sum_edges(arr):
+    # array is shape: mentions, users, orgs
+    mentioned_types, nusers, norgs = arr.shape
+
+    edges = np.zeros((mentioned_types, norgs, norgs), dtype=np.int32)
+
+    for i in np.arange(mentioned_types):
+        for j in np.unique(arr[i].nonzero()[0]):
+            mentions = arr[i, j, :].nonzero()[0]
+            pairs = combinations(mentions, 2)
+            for k,l in pairs:
+                edges[i, k, l] += 1
+
+    return edges
+
+
+# for each user in our list (key)
+# tuple of sets
+# each sets is the ids of all users who ___'d that user
+# then you need to loop through all users afterwards... mess
+
+
+# 3-d array
+# 1d is user (idx lookup needed)
+# 1d is mention-type
+# 1d is mentioned org
+
+# then go through users and assign edges to all combos in array
+
+
+# make separate for all types of edges!!!
+
+
+# def get_edge_weights(tweet_df, user):
+#     '''
+#     for all pairwise combinations of users, this function counts how many users
+#     interact with both users of the respective combination. interaction is divided into
+#     reply to tweets by user, retweet tweety by user or mention user
+
+#     :param pd.DataFrame tweet_df: tweet dataframe with tweet attributes as columns
+#     :param list user: target users
+#     :returns pd.DataFrame res: dataframe with edge counts
+#     '''
+#     user_combs = pairwise_combs(user)
+#     res = []
+#     print('...... Getting edge weights ...')
+#     for u1, u2 in tqdm(user_combs):
+#         w_reply = tweet_df\
+#             [tweet_df['in_reply_to_screen_name'].isin([u1, u2])]\
+#             .groupby('user.screen_name')\
+#             ['in_reply_to_screen_name']\
+#             .nunique()\
+#             .ge(2)\
+#             .sum()
+#         w_retweet = tweet_df\
+#             [tweet_df['retweeted_status.user.screen_name'].isin([u1, u2])]\
+#             .groupby('user.screen_name')\
+#             ['in_reply_to_screen_name']\
+#             .nunique()\
+#             .ge(2)\
+#             .sum()
+#         # w_mention
+#         idxs = outerjoin_lists(
+#             get_idxs_true(isin_listofsets(u1, tweet_df['entities.user_mentions.,screen_name'])),
+#             get_idxs_true(isin_listofsets(u2, tweet_df['entities.user_mentions.,screen_name'])),
+#         )
+#         df_fil = tweet_df.iloc[idxs]
+#         df_fil = df_fil \
+#             .groupby('user.screen_name') \
+#             .apply(lambda x: set.union(*x['entities.user_mentions.,screen_name']))
+#         w_mention = np.array(list(isin_listofsets(u1, df_fil, return_int=True))) \
+#             .dot(list(isin_listofsets(u2, df_fil, return_int=True)))
+#         res += [(u1, u2, w_reply, w_retweet, w_mention)]
+#     res = pd.DataFrame(res)
+#     res.columns = ['user1', 'user2', 'weight replies', 'weight retweets', 'weight mentions']
+#     return res
 
 
 def get_counts(tweet_df, user):
--- test_get_descriptives.py
+++ test_get_descriptives.py
@@ -15,20 +15,19 @@ def tweet_df():
     return pd.read_pickle('test_data/df2.pickle')
 
 
-def test_tweet_df_dtypes(tweet_df):
-    assert tweet_df['created_at'].dtype == 'datetime64[ns, Asia/Kolkata]'
-    assert tweet_df['created_at_D'].dtype == 'datetime64[ns, Asia/Kolkata]'
-    assert all(isinstance(x, str) for x in tweet_df['id_str'])
-    assert all(isinstance(x, str) for x in tweet_df['user.screen_name'])
-    assert tweet_df['user.followers_count'].dtype == 'float64'
-    assert tweet_df['user.friends_count'].dtype == 'float64'
-    assert all(x is None or isinstance(x, str) for x in tweet_df['in_reply_to_status_id_str'])
-    assert all(x is None or isinstance(x, str) for x in tweet_df['in_reply_to_screen_name'])
-    assert all(x is None or isinstance(x, str) for x in tweet_df['retweeted_status.id_str'])
-    assert all(x is None or isinstance(x, str) for x in tweet_df['retweeted_status.user.screen_name'])
-    assert all(x is None or isinstance(x, str) for x in tweet_df['quoted_status.id_str'])
-    assert all(x == {} or is_setofstr(x) for x in tweet_df['entities.user_mentions.,screen_name'])
-
+# def test_tweet_df_dtypes(tweet_df):
+#     assert tweet_df['created_at'].dtype == 'datetime64[ns, Asia/Kolkata]'
+#     assert tweet_df['created_at_D'].dtype == 'datetime64[ns, Asia/Kolkata]'
+#     assert all(isinstance(x, str) for x in tweet_df['id_str'])
+#     assert all(isinstance(x, str) for x in tweet_df['user.screen_name'])
+#     assert tweet_df['user.followers_count'].dtype == 'float64'
+#     assert tweet_df['user.friends_count'].dtype == 'float64'
+#     assert all(x is None or isinstance(x, str) for x in tweet_df['in_reply_to_status_id_str'])
+#     assert all(x is None or isinstance(x, str) for x in tweet_df['in_reply_to_screen_name'])
+#     assert all(x is None or isinstance(x, str) for x in tweet_df['retweeted_status.id_str'])
+#     assert all(x is None or isinstance(x, str) for x in tweet_df['retweeted_status.user.screen_name'])
+#     assert all(x is None or isinstance(x, str) for x in tweet_df['quoted_status.id_str'])
+#     assert all(x == {} or is_setofstr(x) for x in tweet_df['entities.user_mentions.,screen_name'])
 
 
 def test_reply_count_gathers_by_day():
@@ -42,23 +41,26 @@ def test_reply_count_gathers_by_day():
         {'created_at': datetime(2019,3,1), 'in_reply_to_screen_name': 'bar'}
     ]
 
-    counts = reply_count(['foo', 'bar'], tweets)
+    counts = all_counts(['foo', 'bar'], tweets)
+    counts = counts[2]
     ans = {'Wed May 01': {'foo': 1, 'bar': 1}, 'Mon Apr 01': {'foo': 2}, 'Fri Mar 01': {'bar': 1}}
     assert(counts == ans)
 
-def test_mention_count_gathers_by_day():
+
+def test_retweet_count_gathers_by_day():
     tweets = [
-        {'created_at': datetime(2019,5,1), 'entities.user_mentions.,screen_name': ['foo', 'foo']},
-        {'created_at': datetime(2019,5,1), 'entities.user_mentions.,screen_name': ['bar', 'foo', 'qux']},
-        {'created_at': datetime(2019,4,1), 'entities.user_mentions.,screen_name': ['foo']},
-        {'created_at': datetime(2019,4,1), 'entities.user_mentions.,screen_name': ['foo', 'qux']},
-        {'created_at': datetime(2019,4,1), 'entities.user_mentions.,screen_name': ['baz']},
-        {'created_at': datetime(2019,4,1), 'entities.user_mentions.,screen_name': []},
-        {'created_at': datetime(2019,3,1), 'entities.user_mentions.,screen_name': ['bar']}
+        {'created_at': datetime(2019,5,1), 'retweeted_status.user.screen_name': 'foo'},
+        {'created_at': datetime(2019,5,1), 'retweeted_status.user.screen_name': 'bar'},
+        {'created_at': datetime(2019,4,1), 'retweeted_status.user.screen_name': 'foo'},
+        {'created_at': datetime(2019,4,1), 'retweeted_status.user.screen_name': 'foo'},
+        {'created_at': datetime(2019,4,1), 'retweeted_status.user.screen_name': 'baz'},
+        {'created_at': datetime(2019,4,1)},
+        {'created_at': datetime(2019,3,1), 'retweeted_status.user.screen_name': 'bar'}
     ]
 
-    counts = mention_count(['foo', 'bar', 'qux'], tweets)
-    ans = {'Wed May 01': {'foo': 3, 'bar': 1, 'qux': 1}, 'Mon Apr 01': {'foo': 2, 'qux': 1 }, 'Fri Mar 01': {'bar': 1}}
+    counts = all_counts(['foo', 'bar'], tweets)
+    counts = counts[1]
+    ans = {'Wed May 01': {'foo': 1, 'bar': 1}, 'Mon Apr 01': {'foo': 2}, 'Fri Mar 01': {'bar': 1}}
     assert(counts == ans)
 
 
@@ -73,12 +75,13 @@ def test_mention_count_gathers_by_day():
         {'created_at': datetime(2019,3,1), 'entities.user_mentions.,screen_name': ['bar']}
     ]
 
-    counts = mention_count(['foo', 'bar', 'qux'], tweets)
+    counts = all_counts(['foo', 'bar', 'qux'], tweets)[3]
     ans = {'Wed May 01': {'foo': 3, 'bar': 1, 'qux': 1}, 'Mon Apr 01': {'foo': 2, 'qux': 1 }, 'Fri Mar 01': {'bar': 1}}
     assert(counts == ans)
 
 
-def test_original_count():
+
+def test_original_count_gathers_by_day():
     tweets = [
         {'created_at': datetime(2019,5,1), 'user.screen_name': 'foo', 'in_reply_to_status_id_str': 'bar'},
         {'created_at': datetime(2019,5,1), 'user.screen_name': 'foo', 'in_reply_to_status_id_str': 'bar'},
@@ -86,11 +89,13 @@ def test_original_count():
         {'created_at': datetime(2019,5,1), 'user.screen_name': 'foo', 'retweeted_status.id_str': 'bar'},
         {'created_at': datetime(2019,5,1), 'user.screen_name': 'baz'},
         {'created_at': datetime(2019,5,1), 'user.screen_name': 'bar'},
+        {'created_at': datetime(2019,5,2), 'user.screen_name': 'bar'},
     ]
 
-    counts = original_count(['foo', 'bar'], tweets)
-    ans = {'foo': 1, 'bar': 1 }
+    counts = all_counts(['foo', 'bar'], tweets)[0]
+    ans = {'Wed May 01': {'foo': 1, 'bar': 1}, 'Thu May 02': {'bar': 1} }
     assert(counts == ans)
+
 
 def test_follower_count():
     tweets = [
@@ -100,34 +105,86 @@ def test_follower_count():
     ]
 
     counts = follower_count(['foo', 'bar'], tweets)
-    ans = {'foo': {'user.followers_count': 5, 'user.friends_count': 5}, 'bar': {'user.followers_count': 1, 'user.friends_count': 1} }
+    ans = { 'foo': {'user.followers_count': 5, 'user.friends_count': 5},
+            'bar': {'user.followers_count': 1, 'user.friends_count': 1} }
     assert(counts == ans)
 
-def test_flatten_counts_to_df():
-    # Todo test this
-    nested = {'Wed May 01': {'foo': 3, 'bar': 1, 'qux': 1}, 'Mon Apr 01': {'foo': 2, 'qux': 1 }, 'Fri Mar 01': {'bar': 1}}
-    df = flatten_counts_to_df(nested, 'date', 'user')
-    print(df)
-    nested = {'foo': {'user.followers_count': 5, 'user.friends_count': 5}, 'bar': {'user.followers_count': 1, 'user.friends_count': 1} }
-    df = flatten_counts_to_df(nested, 'user', 'measure')
-    print(df)
-    assert(True)
+def test_lookup_user():
+    assert(lookup_user(0, {}, 'foo') == (1, {'foo': 0}, 0))
+    assert(lookup_user(1, {'foo': 0}, 'foo') == (1, {'foo': 0}, 0))
+    assert(lookup_user(1, {'foo': 0}, 'bar') == (2, {'foo': 0, 'bar': 1}, 1))
+    assert(lookup_user(2, {'foo': 0, 'bar': 1}, 'bar') == (2, {'foo': 0, 'bar': 1}, 1))
+
+
+def test_get_mentions():
+    tweet = {'created_at': datetime(2019,5,1), 'user.screen_name': 'foo', 'in_reply_to_screen_name': 'baz', 'entities.user_mentions.,screen_name': ['bar']}
+
+    assert(get_mentions(['bar', 'baz'], tweet) == [(0, ['baz']), (2, ['bar'])])
+
+def test_build_dis():
+    tweets = [
+        {'user.screen_name': 'a', 'created_at': datetime(2019,5,1), 'entities.user_mentions.,screen_name': ['foo', 'foo']},
+        {'user.screen_name': 'a', 'created_at': datetime(2019,5,1), 'entities.user_mentions.,screen_name': ['bar', 'foo', 'qux']},
+        {'user.screen_name': 'a', 'created_at': datetime(2019,4,1), 'entities.user_mentions.,screen_name': ['foo']},
+        {'user.screen_name': 'a', 'created_at': datetime(2019,4,1), 'entities.user_mentions.,screen_name': ['foo', 'qux']},
+        {'user.screen_name': 'b', 'created_at': datetime(2019,4,1), 'entities.user_mentions.,screen_name': ['baz']},
+        {'user.screen_name': 'a', 'created_at': datetime(2019,4,1), 'entities.user_mentions.,screen_name': []},
+        {'user.screen_name': 'a', 'created_at': datetime(2019,3,1), 'entities.user_mentions.,screen_name': ['bar'], 'in_reply_to_screen_name': 'qux'}
+    ]
+
+    orgs = ['foo', 'bar', 'baz', 'qux']
+    lookup = {k:i for i,k in enumerate(orgs)}
+    dis = reduce(build_dis(orgs, lookup), tweets, [{}, {}, {}])
+    assert(dis == [{'a': {3}}, {}, {'a': {0,1,3}, 'b': {2}}])
+    assert(np.all(sum_edges_di(4, dis) == np.array([[[0,0,0,0],
+                                                     [0,0,0,0],
+                                                     [0,0,0,0],
+                                                     [0,0,0,0]],
+                                                    [[0,0,0,0],
+                                                     [0,0,0,0],
+                                                     [0,0,0,0],
+                                                     [0,0,0,0]],
+                                                    [[0,1,0,1],
+                                                     [0,0,0,1],
+                                                     [0,0,0,0],
+                                                     [0,0,0,0]]])))
 
 
 
+# def test_flatten_counts_to_df():
+#     # Todo test this
+#     nested = {'Wed May 01': {'foo': 3, 'bar': 1, 'qux': 1}, 'Mon Apr 01': {'foo': 2, 'qux': 1 }, 'Fri Mar 01': {'bar': 1}}
+#     df = flatten_counts_to_df(nested, 'date', 'user')
+#     print(df)
+#     nested = {'foo': {'user.followers_count': 5, 'user.friends_count': 5}, 'bar': {'user.followers_count': 1, 'user.friends_count': 1} }
+#     df = flatten_counts_to_df(nested, 'user', 'measure')
+#     print(df)
+#     assert(False)
 
-def test_get_edge_weights():
-    tweet_df = pd.DataFrame(
-        {'user.screen_name': ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'a', 'b'],
-         'in_reply_to_screen_name': [1, 2, 1, 2, 1, 1, 2, 1, 3],
-         'retweeted_status.user.screen_name': [1, 2, 1, 2, 1, 1, 2, 1, 3],
-         'entities.user_mentions.,screen_name': [{1, 2, 3}, {1}, {3, 4}, {1}, {2}, {1}, {2}, {1}, {1}]}
-    )
-    edge_weights = pd.DataFrame(
-        {'user1': [1, 1, 1, 2, 2, 3],
-         'user2': [2, 3, 4, 3, 4, 4],
-         'weight replies': [2, 1, 0, 1, 0, 0],
-         'weight retweets': [2, 1, 0, 1, 0, 0],
-         'weight mentions': [2, 2, 1, 1, 0, 1]}
-    )
-    assert get_edge_weights(tweet_df, [1, 2, 3, 4]).equals(edge_weights)
+
+# def test_get_edge_weights():
+#     tweet_df = pd.DataFrame(
+#         {'user.screen_name': ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'a', 'b'],
+#          'in_reply_to_screen_name': [1, 2, 1, 2, 1, 1, 2, 1, 3],
+#          'retweeted_status.user.screen_name': [1, 2, 1, 2, 1, 1, 2, 1, 3],
+#          'entities.user_mentions.,screen_name': [{1, 2, 3}, {1}, {3, 4}, {1}, {2}, {1}, {2}, {1}, {1}]}
+#     )
+
+#     tweets = [
+#         {'created_at': datetime(2019,5,1), 'user.screen_name': 'foo', 'in_reply_to_status_id_str': 'bar'},
+#         {'created_at': datetime(2019,5,1), 'user.screen_name': 'foo', 'in_reply_to_status_id_str': 'bar'},
+#         {'created_at': datetime(2019,5,1), 'user.screen_name': 'foo'},
+#         {'created_at': datetime(2019,5,1), 'user.screen_name': 'foo', 'retweeted_status.id_str': 'bar'},
+#         {'created_at': datetime(2019,5,1), 'user.screen_name': 'baz'},
+#         {'created_at': datetime(2019,5,1), 'user.screen_name': 'bar'},
+#     ]
+
+#     edge_weights = pd.DataFrame(
+#         {'user1': ['foo', 'foo', 'bar'],
+#          'user2': ['bar', 'baz', 'baz'],
+#          'weight replies': [2, 1, 0],
+#          'weight retweets': [2, 1, 0],
+#          'weight mentions': [2, 2, 1]}
+#     )
+
+#     assert get_edge_weights(['foo', 'bar', 'baz'], tweets).equals(edge_weights)
